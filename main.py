a004d73e76bd6600397e502122d17500c7065804
Dominik Bisiakowski <dominik.bisiakowski@makimo.pl>
2022-01-30 16:24:57 +0100

    Add base of logic

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from datetime import datetime
 
 r = random.SystemRandom()
 
+# Use values from https://zaliczenie.aherman.pl/travel
 trips = [
     {
         'name': 'Cancun Bay Resort',
@@ -18,6 +19,25 @@ trips = [
         'country': 'Mexico',
         'stars': 4,
         'price': 450,
+        'start_date': '15.06.2022',
+        'duration': 10,
+        'board': 'All Inclusive'
+    },
+    {
+        'name': 'Cancun Bay Resort',
+        'country': 'Mexico',
+        'stars': 4,
+        'price': 450,
+        'start_date': '15.06.2022',
+        'duration': 14,
+        'board': 'All Inclusive'
+    },
+   {
+        'name': 'Cancun Bay Resort',
+        'country': 'Mexico',
+        'stars': 4,
+        'price': 450,
+        'start_date': '15.06.2022',
         'duration': 7,
         'board': 'All Inclusive'
     },
@@ -26,36 +46,92 @@ trips = [
         'country': 'Mexico',
         'stars': 4,
         'price': 450,
+        'start_date': '15.06.2022',
+        'duration': 10,
+        'board': 'All Inclusive'
+    },
+    {
+        'name': 'Cancun Bay Resort',
+        'country': 'Mexico',
+        'stars': 4,
+        'price': 450,
+        'start_date': '15.06.2022',
+        'duration': 14,
+        'board': 'All Inclusive'
+    },
+    {
+        'name': 'Cancun Bay Resort',
+        'country': 'Mexico',
+        'stars': 4,
+        'price': 450,
+        'start_date': '15.06.2022',
         'duration': 7,
         'board': 'All Inclusive'
     },
     {
         'name': 'Cancun Bay Resort',
-        'country': 'Meksyk',
+        'country': 'Mexico',
         'stars': 4,
         'price': 450,
-        'duration': 7,
+        'start_date': '15.06.2022',
+        'duration': 10,
         'board': 'All Inclusive'
-    }
+    },
+    {
+        'name': 'Cancun Bay Resort',
+        'country': 'Mexico',
+        'stars': 4,
+        'price': 450,
+        'start_date': '15.06.2022',
+        'duration': 14,
+        'board': 'All Inclusive'
+    },
 ]
 
-random_trips = []
+DURATIONS_TO_DISPLAY = [7,10,14]
 
-OFFERS_TO_DISPLAY = 3
+def get_random_trip_by_duration(duration):
+    trips_by_duration = [trip for trip in trips if trip['duration'] == duration]
+    return trips_by_duration[r.randint(0, len(trips_by_duration) - 1)]
 
-for offer_number in range(OFFERS_TO_DISPLAY):
-    while len(random_trips) ==  offer_number:
-        random_trip = r.randint(0, len(trips) - 1)
-        if not random_trip in random_trips:
-            random_trips.append(random_trip)
+trips_to_display = list(map(get_random_trip_by_duration, DURATIONS_TO_DISPLAY))
+
+# 'Please print values from array printed above in human readable format'
+print(trips_to_display)
+
+print('Which tour do you prefer? Select a number from 1 to 3')
+
+picked_trip_number = int(input())
+while picked_trip_number > 3 or picked_trip_number < 1:
+    print('Wrong option. Select a number from 1 to 3')
+    picked_trip_number = int(input())
+
+print('Good job, you picked option')
+picked_trip = trips_to_display[picked_trip_number - 1]
+
+adults = 0
+childrens = 0
+
+print('How many adults')
+
+adults = int(input())
+
+while not isinstance(adults, int):
+    print('Wrong option.')
+    adults = int(input())
 
 
+print('How many childrens')
 
-datetime_object = datetime.strptime('2005.01.22', '%Y.%m.%d')
+childrens = int(input())
 
-print(datetime_object)
+while not isinstance(childrens, int):
+    print('Wrong option.')
+    childrens = int(input())
 
 
-
-print(random_trips)
+print('Full price')
+trip_price = picked_trip['price']
+full_price = trip_price * adults + (trip_price / 2) * childrens
+print(full_price)
 
